8ac9a1b1de4a1da070fddfbd5798b9e994a64416
Jay McEntire <jay.mcentire@gmail.com>
2013-01-09 09:37:59 -0700

    adding speedups

--- dataforms/admin/answeradmin.py
+++ dataforms/admin/answeradmin.py
@@ -1,26 +1,35 @@
 from dataforms.models import AnswerChoice
 from django.contrib import admin
 
-    
+
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ('id', 'submission', 'data_form', 'field', 'field_type', 'value', 'choices')
     #inlines = [AnswerTextInline, AnswerNumberInline, AnswerChoiceInline]
     list_select_related = True
-    
+
     search_fields = ('field__slug', 'field__label')
-    
+
     def __init__(self, *args, **kwargs):
         super(AnswerAdmin, self).__init__(*args, **kwargs)
         self.answer_choices_qs = AnswerChoice.objects.select_related('answer', 'choice').all()
-    
+
+    def queryset(self, request):
+        qs = super(AnswerAdmin, self).queryset(request)
+
+        # use prefetch_related if we can
+        if hasattr(qs, 'prefetch_related'):
+            return qs.prefetch_related('choice')
+        else:
+            return qs
+
     # Get Choices
     def choices(self, obj):
-        return '%s' % str(filter(lambda c: c.answer == obj, self.answer_choices_qs))
+        return '%s' % str(obj.choice.all())
 
     # Get Field Types
     def field_type(self, obj):
         return '%s' % obj.field.field_type
-    
+
     #Hide the model from view
 #    def get_model_perms(self, request):
 #        return {}
@@ -31,7 +40,7 @@ class SubmissionAdmin(admin.ModelAdmin):
     #list_filter = ('')
     search_fields = ('slug',)
     list_select_related = True
-    
+
     def answers_link(self, obj):
         return '<a href="answers/%s/">Answers<a>' % obj.pk
     answers_link.allow_tags = True
