c4c90f69a2c39d3065591db63997321b89c164f3
Jay McEntire <jay.mcentire@gmail.com>
2013-02-12 10:10:41 -0700

    fixing admin forms

--- dataforms/admin/bindingadmin.py
+++ dataforms/admin/bindingadmin.py
@@ -8,34 +8,34 @@ from django.utils import simplejson
 
 
 def ajax_filter(request, object):
-    
+
     #Return 404 if not an ajax request
     if not request.is_ajax():
         raise Http404
-    
+
     values = []
     order = []
     filter = {}
-    
+
     if request.GET.has_key('values'):
         values = request.GET['values'].split(',')
-    
+
     if request.GET.has_key('order'):
         order = request.GET['order'].split(',')
 
     for key, value in request.GET.iteritems():
         if key != 'order' and key !='values':
             filter[str(key)] = str(value)
-    
-    try:    
+
+    try:
         model_class = ContentType.objects.get(app_label="dataforms", model=object).model_class()
         queryset = model_class.objects.values(*values).order_by(*order).filter(**filter)
-    
+
     except:
         return JsonResponse(0)
-        
+
     return JsonResponse(list(queryset))
-    
+
 
 class JsonResponse(HttpResponse):
     """
@@ -47,18 +47,18 @@ class JsonResponse(HttpResponse):
 
 class BindingAdmin(admin.ModelAdmin):
     list_display = ('pk', 'data_form', 'field', 'operator', 'value',
-                    'field_choice', 'true_fields_list', 'false_fields_list')
+                    'field_choice',) #'true_fields_list', 'false_fields_list')
     list_filter = ('data_form__slug',)
     list_select_related = True
     search_fields = ('data_form__title', 'field__slug')
     save_as = True
-    
+
     form = BindingAdminForm
-    
+
     def queryset(self, request):
         qs = super(BindingAdmin, self).queryset(request)
         return qs.select_related('data_form', 'field', 'field_choice__choice', 'field_choice__field')
-    
+
     def true_fields_list(self, obj):
         fields = ''.join([
             '<li>' + c + '</li>' for c in obj.true_field
@@ -80,7 +80,7 @@ class BindingAdmin(admin.ModelAdmin):
         ]) if obj.false_choice else ''
         return '<ul>' + fields + choices + '</ul>'
     false_fields_list.allow_tags = True
-    
+
 
     def get_urls(self):
         urls = super(BindingAdmin, self).get_urls()
@@ -89,8 +89,8 @@ class BindingAdmin(admin.ModelAdmin):
              self.admin_site.admin_view(ajax_filter, cacheable=True)),
         )
         return new_urls + urls
-    
-    
+
+
     class Media:
         js = ADMIN_JS
         css = { 'all' : ADMIN_CSS }
